69136927d15b340c02f73b9ab82c22917f4456fb
Kirsten <kegillam@ucdavis.edu>
2019-03-20 19:37:43 -0700

    playing around

--- facet_file_generator.py
+++ facet_file_generator.py
@@ -1,19 +1,27 @@
 from shutil import copyfile
-num_chars = str(3) ##TODO: automate this? and instance file?
+num_chars = str(12) ##TODO: automate this? and instance file?
+#num_chars = str(3) ##TODO: automate this? and instance file?
 
 facet_names = []
 
 #get requested facets
-new_facet = input("enter the name of your first desired facet:")
-while new_facet:
-    facet_names.append(new_facet)
-    new_facet = input("enter the name of your next desired facet, or press enter to generate files:")
+with open("facets.txt", 'r') as file:
+    filedata = file.read()
+    facet_names = filedata.rstrip('\n').split('\n')
+    
+#new_facet = input("enter the name of your first desired facet:")
+#while new_facet:
+#    facet_names.append(new_facet)
+#    new_facet = input("enter the name of your next desired facet, or press enter to generate files:")
 
 # Read in the template file
 with open('templates/facet_template.lp', 'r') as file :
-  filedata = file.read()
+    filedata = file.read()
 
 for facet_name in facet_names:
+    if facet_name == "":
+        continue
+
     # Replace the target string
     tempfiledata = filedata.replace('template', facet_name)
 
@@ -40,16 +48,16 @@ with open("generated/similarity_generated.lp", 'w') as file:
     final_string = final_string + "E" + str(i-1) + "!=T."
     file.write(final_string)
 
-with open("generated/similarity_instance.lp", 'w') as file:
-    #file.write("#show pair_similarity/3.\n")
-    file.write("%problem instance\n")
-    file.write("human(1.." + num_chars + ").\n\n")
-    file.write("%add more rules here\n")
-    file.write("%example:\npair_similarity(1,2,2).\n")
+#with open("generated/similarity_instance.lp", 'w') as file:
+#    #file.write("#show pair_similarity/3.\n")
+#    file.write("%problem instance\n")
+#    file.write("human(1.." + num_chars + ").\n\n")
+#    file.write("%add more rules here\n")
+#    file.write("%example:\npair_similarity(1,2,2).\n")
 
 copyfile("similarity_persistant.lp", "generated/similarity_persistant.lp")
 copyfile("affinity.lp", "generated/affinity.lp")
-
-
+copyfile("amelia.lp", "generated/amelia.lp")
+copyfile("facet_affinity.lp", "generated/facet_affinity.lp")
 
 
